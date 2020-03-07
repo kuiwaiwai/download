c65cf351924bce4e337be9829ea0549e83baedea
kuiwaiwai <56921773+kuiwaiwai@users.noreply.github.com>
2020-03-07 19:28:49 +0800

    Add files via upload

--- js/sousuo.js
+++ js/sousuo.js
@@ -1,6 +1,6 @@
 <script>
 function sousuo(){
 var kss = document.getElementById("kss");
-window.location.href="https://kuiwaiwai.github.io/download/"+kss;
+window.location.href="https://kdownload.top/download/"+kss;
 }
  </script>
